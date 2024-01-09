4d77654d9ca0931847448b54bb7f1e111c3dbb03
gary92gs <gary92.gs@gmail.com>
2024-01-08 17:59:52 -0700

    finished assertEqual activity

---
+++ assertEqual.js
@@ -0,0 +1,6 @@
+const assertEqual = (actual,expected) => {
+  ((actual === expected) ? console.log('👍 Assertion Passed: [actual] === [expected]') : console.log('👎 Assertion Failed: [actual] !== [expected]'));
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
