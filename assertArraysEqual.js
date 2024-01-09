51650117ecc5387f0be44bcd414095ef540cb1ca
gary92gs <gary92.gs@gmail.com>
2024-01-09 16:11:55 -0700

    created better version of assert that works for arrays, using 2 functions

---
+++ assertArraysEqual.js
@@ -0,0 +1,21 @@
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArrayEquals = (arr1,arr2) => {
+  ((eqArrays(arr1,arr2)) ? console.log(`👍 Assertion Passed: ${arr1} === ${arr2}`) : console.log(`👎 Assertion Failed: ${arr2} !== ${arr2}`));
+};
+
+assertArrayEquals([1, 2, 3], [1, 2, 3]); // => true
+assertArrayEquals([1, 2, 3], [3, 2, 1]); // => false
+
+assertArrayEquals(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArrayEquals(["1", "2", "3"], ["1", "2", 3]); // => false
