76b3cc53c0037d49c68cb83dfc63a1f560294f1f
gary92gs <gary92.gs@gmail.com>
2024-01-11 18:21:27 -0700

    improved findKeyByValue by implementing callback functions

---
+++ findKey.js
@@ -0,0 +1,29 @@
+const assertEqual = (actual,expected) => {
+  ((actual === expected) ? console.log(`👍 Assertion Passed: ${actual} === ${expected}`) : console.log(`👎 Assertion Failed: ${actual} !== ${expected}`));
+};
+
+const findKey = (object,callback) => {
+  for (const item in object) {
+    if (callback(object[item])){
+      return item;
+    }
+  }
+  return undefined;
+}
+
+const ratings = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}
+
+result1 = findKey(ratings, x => x.stars === 2);
+result2 = findKey(ratings, item => item.stars === 4);
+result3 = findKey(ratings, item => item.stars === 3);
+
+assertEqual(result1,'noma');
+assertEqual(result2, undefined);
+assertEqual(result3,'Akaleri');
