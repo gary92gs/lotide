eacd181821b65675eba7f79bb8a727fff13879bf
gary92gs <gary92.gs@gmail.com>
2024-01-15 10:00:59 -0700

    forgot to remove test cases on tail.js, they were supposed to be only present in tailTest.js

--- tail.js
+++ tail.js
@@ -6,13 +6,6 @@ const tail = (array) => {
   return array.slice(1);
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-const wordsLength = words.length; // stores original length of words array (to ensure tail is not changing it)
-const result = tail(words);
-assertEqual(wordsLength, words.length); //checks that the length of the words array is unchanged after calling tail() [even though the for loop will not work otherwise - see below]
-for (let i = 0; i < result.length; i++) {
-  assertEqual(words[i + 1], result[i]); //see here: words[i+1] vs result[i]
-}
 
 module.exports = tail;
 
