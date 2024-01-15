56ae6ce0f4a534569a671673ae3e05fa27c2a838
gary92gs <gary92.gs@gmail.com>
2024-01-15 10:39:58 -0700

    added test case file for assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,7 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
-const assertArrayEquals = (arr1,arr2) => {
+const assertArraysEqual = (arr1,arr2) => {
   ((eqArrays(arr1,arr2)) ? console.log(`👍 Assertion Passed: ${arr1} === ${arr2}`) : console.log(`👎 Assertion Failed: ${arr1} !== ${arr2}`));
 };
 
-assertArrayEquals([1, 2, 3], [1, 2, 3]); // => true
-assertArrayEquals([1, 2, 3], [3, 2, 1]); // => false
-
-assertArrayEquals(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArrayEquals(["1", "2", "3"], ["1", "2", 3]); // => false
+module.exports = assertArraysEqual;
